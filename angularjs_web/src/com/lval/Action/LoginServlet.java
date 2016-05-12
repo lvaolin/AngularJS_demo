007283ca00e221365d0cd82254b8d78bc1ec552e
lval <lval@chanjet.com>
2016-05-12 19:14:12 +0800

    学习过程中完整的demo，已入门，待深入学习

---
+++ angularjs_web/src/com/lval/Action/LoginServlet.java
@@ -0,0 +1,31 @@
+package com.lval.Action;
+
+import net.sf.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by admin on 2016/5/12.
+ */
+@WebServlet(name = "LoginServlet",urlPatterns = "/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request,response);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        JSONObject obj = new JSONObject();
+        obj.put("echoinfo","ECHO:"+request.getParameter("info"));
+        response.getWriter().print(obj);
+    }
+}
